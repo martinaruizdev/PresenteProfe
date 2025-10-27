c8f6aed8ac2d317a9c3790c930e05432ca312e05
martinaruizdev <martinaruizdev@gmail.com>
2025-10-27 16:44:14 -0300

    feat: Agregar estructura básica del Home

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Button from '../Button/Button'
+
+const Header = () => {
+  return (
+    <>
+    <Navbar/>
+    <h1>Tomá lista en segundos, sin papel ni estrés.</h1>
+    <h2>Con PresenteProfe, tus estudiantes registran su asistencia escaneando un código QR. Simple, rápido y confiable.</h2>
+    <Button>Probar Ahora</Button>
+    </>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Login</li>
+            <li>Registro</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
