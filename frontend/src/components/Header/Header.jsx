1d12e3ec019ad568b1e296b47438ebb0b4471d99
martinaruizdev <martinaruizdev@gmail.com>
2025-10-27 17:58:14 -0300

    feat: Agregar estilos a componentes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Button from '../Button/Button'
+import fondo from '../../assets/fondo.png';
 
 const Header = () => {
   return (
     <>
+    <div id='hero-header' style={{ backgroundImage: `url(${fondo})` }} className='bg-[url("/fondo.png")] min-h-screen flex flex-col items-center bg-cover bg-center'>
     <Navbar/>
-    <h1>Tomá lista en segundos, sin papel ni estrés.</h1>
+    <div className='flex-1 flex flex-col justify-center items-start h-screen'>
+    <h1 className='text-4xl font-bold text-dark'>Tomá <span className='text-blue-950'>lista en segundos</span> , sin papel ni estrés.</h1>
     <h2>Con PresenteProfe, tus estudiantes registran su asistencia escaneando un código QR. Simple, rápido y confiable.</h2>
     <Button>Probar Ahora</Button>
-    </>
+    </div>
+    </div>
+     </>
   )
 }
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,29 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <ul>
-            <li>Home</li>
-            <li>Login</li>
-            <li>Registro</li>
-        </ul>
+<nav className="flex justify-center py-6">
+      <ul className="flex space-x-6 bg-white/80 backdrop-blur-md rounded-full px-8 py-3 shadow-lg">
+        <li>
+          <a href="#home" className="text-gray-700 hover:text-blue-500 font-medium">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="text-gray-700 hover:text-blue-500 font-medium">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#services" className="text-gray-700 hover:text-blue-500 font-medium">
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="text-gray-700 hover:text-blue-500 font-medium">
+            Contact
+          </a>
+        </li>
+      </ul>
     </nav>
   )
 }
